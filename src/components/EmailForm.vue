39b4ef154b4c17809f0d96e84894756e41c07993
Martin Hell <martinhellapp@gmail.com>
2022-09-21 16:15:29 +0200

    contact form

---
+++ src/components/EmailForm.vue
@@ -0,0 +1,110 @@
+<template>
+
+    <div class="section">
+      <h1>Contact Me</h1>
+      <div class="container">
+          <form>
+            <label>Name</label>
+            <input
+              type="text"
+              v-model="name"
+              name="name"
+              placeholder="Your Name"
+            >
+            <label>Email</label>
+            <input
+              type="email"
+              v-model="email"
+              name="email"
+              placeholder="Your Email"
+              >
+            <label>Message</label>
+            <textarea
+              name="message"
+              v-model="message"
+              cols="30" rows="5"
+              placeholder="Message">
+            </textarea>
+            <input type="submit" value="Send">
+          </form>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+* {box-sizing: border-box;}
+
+.container {
+    display: block;
+    margin:auto;
+    text-align: center;
+    border-radius: 5px;
+    border-style: solid;
+    border-color: rgba(255, 255, 255, 0.548);
+    border-width: 1px;
+    background-color: #11111111;
+    padding: 20px;
+    width: 50%;
+}
+
+label {
+    float: left;
+    color: rgb(214, 214, 214);
+}
+
+input[type=text], [type=email], textarea {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+    margin-top: 6px;
+    margin-bottom: 16px;
+    resize: vertical;
+}
+
+input[type=submit] {
+    background-color: #c20903;
+    color: white;
+    padding: 12px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+input[type=submit]:hover {
+    background-color: #d4514c;
+}
+</style>
+
+<script>
+import emailjs from 'emailjs-com'
+
+export default {
+  data () {
+    return {
+      name: '',
+      email: '',
+      message: ''
+    }
+  },
+  methods: {
+    sendEmail (e) {
+      try {
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,
+          'YOUR_USER_ID', {
+            name: this.name,
+            email: this.email,
+            message: this.message
+          })
+      } catch (error) {
+        console.log({ error })
+      }
+      // Reset form field
+      this.name = ''
+      this.email = ''
+      this.message = ''
+    }
+  }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,12 +2,14 @@
   <NavigationBar/>
   <ProfileBox></ProfileBox>
   <ProjectsSection/>
+  <EmailForm></EmailForm>
 </template>
 
 <script>
 import ProjectsSection from '@/components/ProjectsSection.vue'
 import ProfileBox from '@/components/ProfileBox.vue'
 import NavigationBar from '@/components/NavigationBar.vue'
+import EmailForm from '@/components/EmailForm.vue'
 // @ is an alias to /src
 
 export default {
@@ -15,7 +17,8 @@ export default {
   components: {
     ProjectsSection,
     ProfileBox,
-    NavigationBar
+    NavigationBar,
+    EmailForm
   }
 }
 </script>
