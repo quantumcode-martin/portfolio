5ca098a3f8b50dd06bcef5e1a6fc969d481315ae
QuantumMartin <martinhellapp@gmail.com>
2022-10-09 00:27:57 +0200

    trying prerender

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,27 @@
+const plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  // Only import when needed
+  const { join } = require('path')
+  const PrerenderPlugin = require('prerender-spa-plugin')
+  const Renderer = PrerenderPlugin.PuppeteerRenderer
+
+  // Add the prerender plugin to the plugins array
+  plugins.unshift(
+    new PrerenderPlugin({
+      staticDir: join(__dirname, 'dist'), // Vue's build folder
+      routes: ['/'], // Only render the homepage
+      maxConcurrentRoutes: 10,
+      renderer: new Renderer({
+        headless: true,
+        renderAfterTime: 100,
+        executablePath: '/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe' }) // Configure the renderer with all default options
+    })
+  )
+}
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: { plugins }
 })
