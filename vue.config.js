f3018df52ac120ac6aea352d36276852cd4cadcf
QuantumMartin <martinhellapp@gmail.com>
2022-10-09 14:48:02 +0200

    prerender for all project pages

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,20 @@
+
 const plugins = []
 
 if (process.env.NODE_ENV === 'production') {
   // Only import when needed
   const { join } = require('path')
   const PrerenderPlugin = require('prerender-spa-plugin-next')
+  const projectsData = require(join(__dirname, 'src/assets/projects/projects.json'))
+
+  const projectRoutes = projectsData.map(p => '/project/' + p.url_name)
+  projectRoutes.push('/')
 
   // Add the prerender plugin to the plugins array
   plugins.unshift(
     new PrerenderPlugin({
       staticDir: join(__dirname, 'dist'), // Vue's build folder
-      routes: ['/', '/project/reignscape'], // Only render the homepage
+      routes: projectRoutes, // Only render the homepage
       renderAfterTime: 5000
     })
   )
